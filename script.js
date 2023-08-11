dcb3bac1b5e87481e5c50dafb3ebdfdb37b03a6b
rncraven94 <neilcraven2013@gmail.com>
2023-08-11 15:11:52 -0400

    script.js

--- script.js
+++ script.js
@@ -6,5 +6,11 @@ const outputElement = document.getElementById("output");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   const transactionValue = document.getElementById("transaction").value;
-  outputElement.textContent = `Transaction Amount: $${transactionValue}`;
+  //creating anew element to hold the transaction data
+  const transactionItem = document.createElement("div");
+  transactionItem.textContent = `Transaction Amount: $${transactionValue}`;
+  // append the new transaction item to the output container
+  outputElement.appendChild(transactionItem);
+  //clear the input field after submission
+  document.getElementById("transaction").value = "";
 });
