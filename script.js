d0ec976a6a9e2d1934ad2b8f1462a17167ad75b8
rncraven94 <neilcraven2013@gmail.com>
2023-08-12 19:08:21 -0400

    script.js

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ use: "strict";
 
 const form = document.getElementById("myForm");
 const outputElement = document.getElementById("output");
+const totalElement = document.getElementById("total");
+let totalArr = [];
+let total = 0;
+
+//updating the total
+function updateTotal() {
+  total = totalArr.reduce((sum, value) => sum + parseFloat(value), 0);
+  totalElement.textContent = `Total: $${total.toFixed(2)}`;
+}
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -9,8 +18,11 @@ form.addEventListener("submit", function (e) {
   //creating anew element to hold the transaction data
   const transactionItem = document.createElement("div");
   transactionItem.textContent = `Transaction Amount: $${transactionValue}`;
+  totalArr.push(transactionValue);
   // append the new transaction item to the output container
   outputElement.appendChild(transactionItem);
   //clear the input field after submission
   document.getElementById("transaction").value = "";
+
+  updateTotal();
 });
