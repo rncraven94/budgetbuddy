267e355068e9f1ff7d6b27c329911d05b282d8c4
rncraven94 <neilcraven2013@gmail.com>
2023-08-13 16:24:51 -0400

    script.js

--- script.js
+++ script.js
@@ -16,22 +16,35 @@ function updateTotal() {
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   const transactionValue = document.getElementById("transaction").value;
+  const transactionType = document.getElementById("transactionType").value;
   //creating anew element to hold the transaction data
   const transactionItem = document.createElement("div");
 
   //checking if above 0
   if (transactionValue < 0) {
-    transactionItem.textContent = `Transaction Amount: $${transactionValue}`;
+    transactionItem.textContent = `Transaction Type: ${transactionType}, Transaction Amount: $${transactionValue}`;
     transactionItem.classList.add("red-text");
   } else {
-    transactionItem.textContent = `Transaction Amount: $${transactionValue}`;
+    transactionItem.textContent = `Transaction Type: ${transactionType}, Transaction Amount: $${transactionValue}`;
   }
 
+  //delete a row
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "X";
+  deleteButton.addEventListener("click", function () {
+    const index = Array.from(outputElement.children).indexOf(transactionItem);
+    totalArr.splice(index, 1);
+    outputElement.removeChild(transactionItem);
+    updateTotal();
+  });
+
+  transactionItem.appendChild(deleteButton);
   totalArr.push(transactionValue);
   // append the new transaction item to the output container
   outputElement.appendChild(transactionItem);
   //clear the input field after submission
   document.getElementById("transaction").value = "";
+  document.getElementById("transactionType").value = "";
 
   updateTotal();
 });
